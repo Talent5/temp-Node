456c95749e0c2169e395fd1481a1d0db593041ad
Talent Mundwa <talent.mundwa@students.uz.ac.zw>
2024-10-16 07:38:54 +0200

    Add .gitignore file to ignore /node_modules

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+//Modules
+const names = require('./4-names')
+const sayHi = require('./5-utilis')
+const data = require('./6-alternativ-flavour')
+require('./7-mind-grenade')
+sayHi('Susan')
+sayHi(names.Talent)
+sayHi(names.Peter)
---
+++ 4-names.js
@@ -0,0 +1,6 @@
+const secret = 'SUPER SECREAT'
+const Talent = 'Talent'
+const Peter = 'Peter'
+
+
+module.exports = {Talent, Peter}
