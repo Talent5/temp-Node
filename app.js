eb1d6f4b9840e3c7af075124e85435e32bb822b0
Talent Mundwa <talent.mundwa@students.uz.ac.zw>
2024-10-17 18:29:51 +0200

    Add event loop examples and server code

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const _ = require('lodash')
+const http = require ('http')
 
+const server = http.createServer((req, res) => {
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+    if (req.url === '/') {
+        res.end('Welcome to our home page')
+    }
+    if (req.url === '/about') {
+        res.end('Here is our short history')
+    }
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>
+    `)
+}
+)
+
+server.listen(5000, () =>{
+    
+    console.log('Server listening on port : 5000....')})
